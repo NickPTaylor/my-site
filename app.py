6eb8af976e5c8d5f7972b739560c5875826caaa0
NickPTaylor <nick.monkey@googlemail.com>
2020-06-19 14:14:32 +0100

    General refactoring
    - Add favicons
    - Update file/directory names

--- app.py
+++ app.py
@@ -4,17 +4,13 @@ app = Flask(__name__)
 app.config['FREEZER_RELATIVE_URLS'] = True
 
 @app.route('/')
-@app.route('/home.html')
+@app.route('/index.html')
 def home():
-    return render_template('home.html')
+    return render_template('index.html', title="Home")
 
 @app.route('/cv.html')
 def cv():
-    return render_template('cv.html')
-
-@app.route('/about_me.html')
-def about_me():
-    return render_template('about_me.html')
+    return render_template('cv.html', title="CV")
 
 
 if __name__ == '__main__':
